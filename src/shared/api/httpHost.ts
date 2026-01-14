ad3f2d60cb762b525eaf128f86294083e4b8cc64
wqr1 <dimakond520@gmail.com>
2026-01-14 19:04:27 +0300

    feat: http error handling

--- src/shared/api/httpHost.ts
+++ src/shared/api/httpHost.ts
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 import { authModel } from "@/entities/auth";
 import { API_URL } from "../config";
+import { notificationModel } from "@/entities/notifications";
 
 export const $httpHost = axios.create({
   baseURL: API_URL,
@@ -19,3 +20,32 @@ $httpHost.interceptors.request.use(function (config) {
 
   return config;
 });
+
+$httpHost.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const ogReq: AxiosError = error;
+
+    notificationModel.notify({
+      title: `HTTP Ошибка${": " + ogReq.response?.status || ""}`,
+      text: `${
+        (ogReq.response?.data as { detail?: string })?.detail || ogReq.message
+      }`,
+    });
+
+    // if (error.response?.status >= 400 && !ogReq._retry) {
+    //   ogReq._retry = true;
+    //   try {
+    //     const refreshResponse = await $httpHost.post("/refresh");
+    //     const { accessToken } = refreshResponse.data;
+    //     authModel.setAccessToken(accessToken);
+    //     ogReq.headers.Authorization = `Bearer ${accessToken}`;
+    //     return $httpHost(ogReq);
+    //   } catch (refreshError) {
+    //     authModel.setAccessToken(null);
+    //     return Promise.reject(refreshError);
+    //   }
+    // }
+    return Promise.reject(error);
+  }
+);
